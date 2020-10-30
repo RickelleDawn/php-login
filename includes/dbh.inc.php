e39c8fc7a2c165e56bc9be3704829637daaab76f
Rickelle Tackitt <rickelledawn@gmail.com>
2020-10-29 23:07:20 -0500

    db test

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$serverName = "localhost";
-$dbUsername = "root";
-$dbPassword = "localtest";
-$dbName = "loginSystem";
+$serverName = "us-cdbr-east-02.cleardb.com";
+$dbUsername = "b619a027df8490";
+$dbPassword = "3db55ad9";
+$dbName = "heroku_c8de46839055606";
 
 
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
